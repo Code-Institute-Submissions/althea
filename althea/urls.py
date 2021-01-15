72f068eb1329cbe2a18d7901645731e589846031
gideongannaban <gideongannaban1985@gmail.com>
2021-01-15 16:31:50 +0000

    Added shopping bag app, urls and template

--- althea/urls.py
+++ althea/urls.py
@@ -22,5 +22,6 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('accounts/', include('allauth.urls')),
     path('', include('home.urls')),
-    path('store/', include('products.urls'))
+    path('store/', include('products.urls')),
+    path('bag/', include('bag.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
