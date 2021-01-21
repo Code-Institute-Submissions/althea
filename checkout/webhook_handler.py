ad6d76fc870e6eb853bc356b66a93e15c6b6af8b
gideongannaban <gideongannaban1985@gmail.com>
2021-01-21 23:15:17 +0000

    Updated webhook handler to hanlde profiles

--- checkout/webhook_handler.py
+++ checkout/webhook_handler.py
@@ -2,6 +2,7 @@ from django.http import HttpResponse
 
 from .models import Order, OrderLineItem
 from products.models import Product
+from profiles.models import UserProfile
 
 import json
 import time
@@ -39,6 +40,21 @@ class StripeWH_Handler:
             if value == "":
                 shipping_details.address[field] = None
 
+        # Update profile information if save_info was checked
+        profile = None
+        username = intent.metadata.username
+        if username != 'AnonymousUser':
+            profile = UserProfile.objects.get(user__username=username)
+            if save_info:
+                profile.phone_number = shipping_details.phone,
+                profile.country = shipping_details.address.country,
+                profile.province = shipping_details.address.state,
+                profile.town_or_city = shipping_details.address.city,
+                profile.street_address = shipping_details.address.line2,
+                profile.house_number = shipping_details.address.line1,
+                profile.default_county = shipping_details.address.state
+                profile.save()
+
         order_exists = False
         attempt = 1
         while attempt <= 5:
@@ -70,13 +86,14 @@ class StripeWH_Handler:
             try:
                 order = Order.objects.create(
                     full_name=shipping_details.name,
+                    user_profile=profile,
                     email=billing_details.email,
                     phone_number=shipping_details.phone,
                     country=shipping_details.address.country,
                     province=shipping_details.address.state,
                     town_or_city=shipping_details.address.city,
-                    street_address=shipping_details.address.line2,                    
-                    house_number=shipping_details.address.line1,                    
+                    street_address=shipping_details.address.line2,
+                    house_number=shipping_details.address.line1,
                     original_bag=bag,
                     stripe_pid=pid,
                 )
