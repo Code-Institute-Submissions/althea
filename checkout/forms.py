408f132d76d26ac29d0a53f8c264f5b179188225
gideongannaban <gideongannaban1985@gmail.com>
2021-01-21 14:45:28 +0000

    Added CountryField and updated checkout css

--- checkout/forms.py
+++ checkout/forms.py
@@ -23,7 +23,6 @@ class OrderForm(forms.ModelForm):
             'building_name': 'Building Name',
             'house_number': 'House Number',
             'street_address': 'Street Address',
-            'country': 'Country',
             'town_or_city': 'Town or City',
             'province': 'Province',
             'barangay': 'Barangay',
@@ -32,10 +31,12 @@ class OrderForm(forms.ModelForm):
 
         self.fields['full_name'].widget.attrs['autofocus'] = True
         for field in self.fields:
-            if self.fields[field].required:
-                placeholder = f'{placeholders[field]} *'
-            else:
-                placeholder = placeholders[field]
-            self.fields[field].widget.attrs['placeholder'] = placeholder
+            if field != 'country':
+                if self.fields[field].required:
+                    placeholder = f'{placeholders[field]} *'
+                else:
+                    placeholder = placeholders[field]
+                self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'stripe-style-input'
             self.fields[field].label = False
+
