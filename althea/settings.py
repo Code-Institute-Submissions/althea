3dd329ca2c85d84424b1fdb0d98b3890a91c13c2
gideongannaban <gideongannaban1985@gmail.com>
2021-01-24 13:56:47 +0000

    Updated DATABASE_URL in env settings

--- althea/settings.py
+++ althea/settings.py
@@ -120,6 +120,13 @@ WSGI_APPLICATION = 'althea.wsgi.application'
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#    'default': {
+#        'ENGINE': 'django.db.backends.sqlite3',
+#        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#    }
+# }
+
 if 'DATABASE_URL' in os.environ:
     DATABASES = {
         'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
