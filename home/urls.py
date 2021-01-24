ff5d7da3ffb1761e95229ccd601501cbc1ee6ed9
gideongannaban <gideongannaban1985@gmail.com>
2021-01-24 23:33:43 +0000

    Updated CSS and added and about views and url

--- home/urls.py
+++ home/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .views import about_us
 
 urlpatterns = [
     path('', views.index, name='home'),
+    path('about_us/', about_us, name='about_us'),
 ]
--- home/views.py
+++ home/views.py
@@ -1,9 +1,12 @@
 from django.shortcuts import render
 
-# Create your views here.
-
 
 def index(request):
     """A view to return the index page """
 
     return render(request, 'home/index.html')
+
+def about_us(request):
+    """ A view to return to the about us page """
+
+    return render(request, "home/about_us.html")
