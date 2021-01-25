fb0b0577dab9f0a6b8b8ad528417fab3be14c4a3
gideongannaban <gideongannaban1985@gmail.com>
2021-01-25 18:23:20 +0000

    Updated CSS for bags and checkout

--- althea/settings.py
+++ althea/settings.py
@@ -209,7 +209,7 @@ STRIPE_WH_SECRET = os.getenv('STRIPE_WH_SECRET', '')
 
 if 'DEVELOPMENT' in os.environ:
     EMAIL_BACKEND = 'django.core.mail.backends.console.EmailBackend'
-    DEFAULT_FROM_EMAIL = 'althea@example.com'
+    DEFAULT_FROM_EMAIL = 'althea.online.store@gmail.com'
 else:
     EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
     EMAIL_USE_TLS = True
